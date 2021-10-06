9ee1cb542008cca1bf493dd8e7f7000f03615556
Samouczek <795kasia@gmail.com>
2021-10-06 13:59:49 +0200

    add structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {MdAccountCircle, MdNotificationsNone, MdSearch} from "react-icons/all";
 
 const Navbar = () => {
     return (
-        <div>
-            Home Test
-        </div>
+        <nav className='container navigation'>
+            <ul className ='menu-left'>
+                <li className='menu-left-item menu-left-logo'>
+                    <div className='menu-left-logo-frame'> <div className='menu-left-logo-circle'> </div></div>
+                </li>
+                <li className='menu-left-item'>Wydarzenia</li>
+                <li className='menu-left-item'>Konatkt</li>
+                <li className='menu-left-item'>Wesprzyj Nas</li>
+            </ul>
+            <ul className ='menu-right'>
+                <li className='menu-right-item menu-right-notifications-icon'><MdSearch /></li>
+                <li className='menu-right-item menu-right-profile-icon'><MdNotificationsNone /></li>
+                <li className='menu-right-item'><MdAccountCircle /></li>
+                <li className='menu-right-item menu-right-search-icon'> </li>
+            </ul>
+        </nav>
     );
 };
 
